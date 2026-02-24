4ed1acd8a50882f1fbc2da2582ba014c3fdc6310
Alejandro-M-P <jy4fwdd4b7@privaterelay.appleid.com>
2026-02-24 18:21:03 +0100

    completar endpoint de listado para el panel de administración

--- backend/src/main/java/es/hitro/backend/controller/ProductoController.java
+++ backend/src/main/java/es/hitro/backend/controller/ProductoController.java
@@ -1,4 +1,27 @@
 package es.hitro.backend.controller;
 
-public class ProductoControler {
+import es.hitro.backend.entity.Producto;
+import es.hitro.backend.service.ProductoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/productos")
+
+public class ProductoController {
+
+    private final ProductoService productoService;
+    public ProductoController(ProductoService productoService) {
+        this.productoService = productoService;
+    }
+
+
+
+    @GetMapping
+    public List<Producto> listar() {
+        return productoService.obtenerTodos();
+    }
 }
